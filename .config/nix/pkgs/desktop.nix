db3668aa64e72aa74e9af0e12b8206e985401462
Emilia Daelman <emilia.daelman@gmail.com>
2025-08-29 12:41:58 +0200

    remove unused configs and add anime4k to mpv config

--- .config/nix/pkgs/desktop.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, lib, ... }:
-
-lib.mkIf (config.networking.hostName == "milly-nixos-desktop") {
-  # desktop-specific config
-  environment.systemPackages = with pkgs; [
-    cowsay
-  ];
-}
--- .config/nix/pkgs/laptop.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, lib, ... }:
-
-lib.mkIf (config.networking.hostName == "milly-nixos-laptop") {
-  # laptop-specific config
-  environment.systemPackages = with pkgs; [
-  ];
-}
